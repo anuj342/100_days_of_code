8ffe91dc06116e1929cfbe89641cd7491b3e4093
anuj342 <anujnalawade644@gmail.com>
2025-08-29 23:13:30 +0530

    Blackjack Game

--- blackjack.py
+++ blackjack.py
@@ -1,22 +1,11 @@
 import random
 
-skeleton = """
-.------.            _     _            _    _            _    
-|A_  _ |.          | |   | |          | |  (_)          | |   
-|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
-| \  /|K /\  |     | '_ \| |/ _' |/ __| |/ / |/ _' |/ __| |/ /
-|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
-'-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\
-      |  \/ K|                            _/ |                
-      '------'                           |__/ """
-
 def blackjack():
     cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
     user_cards = []
     dealer_cards = []
     choice = input("Do you want to play blackjack? (y/n):")
     while choice == "y":
-        print(skeleton)
         user_cards = random.sample(cards, 2)
         dealer_cards = random.sample(cards, 2)
         print(f"Your cards are: {user_cards}")
@@ -32,12 +21,16 @@ def blackjack():
             print(f"The sum of your cards are: {sum(user_cards)}")
             print(f"The dealer's first card are: {dealer_cards[0]}")
             print(f"The sum of dealers cards are: {sum(dealer_cards)}")
-            if sum(user_cards) < sum(dealer_cards):
+            if sum(user_cards) > 21:
+                print("You lose")
+            elif sum(dealer_cards) > 21:
+                print("Dealer losses, You win")
+            elif sum(user_cards) < sum(dealer_cards):
                 print("Dealer wins!")
             elif sum(user_cards) > sum(dealer_cards):
                 print("User wins!")
             elif sum(user_cards) == 21 and sum(dealer_cards) == 21:
-                print("You win")
+                print("Its a draw.")
             else:
                 print("Error")
             break
